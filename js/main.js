ec49d4876bf53006755b09b25014b3c4ebf8e898
diqost <sapozhkov2008@yandex.ru>
2016-07-24 15:41:55 +0300

    initCommit

--- js/main.js
+++ js/main.js
@@ -10,3 +10,16 @@ window.onload = function() {
         }
     });
 };
+document.addEventListener("rotarydetent", function(ev) {
+	var dir = ev.detail.direction;
+	if (dir === "CW") {
+		map.zoomIn();
+
+	} else if (dir === "CCW") {
+
+		map.zoomOut();
+
+	} else {
+		alert("SMTH WRONG");
+	}
+});
