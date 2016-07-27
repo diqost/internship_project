4f158e983239d296b14fe4b40368c96575bf0b00
diqost <sapozhkov2008@yandex.ru>
2016-07-27 20:16:59 +0300

    add routes, debug fixes

--- js/main.js
+++ js/main.js
@@ -1,12 +1,35 @@
 window.onload = function() {
-    // TODO:: Do your initialization job
+	// TODO:: Do your initialization job
 
-    // add eventListener for tizenhwkey
-    document.addEventListener('tizenhwkey', function(e) {
-        if (e.keyName === "back") {
-            try {
-                tizen.application.getCurrentApplication().exit();
-            } catch (ignore) {}
-        }
-    });
+	// add eventListener for tizenhwkey
+	window
+			.addEventListener(
+					'tizenhwkey',
+					function(ev) {
+						if (ev.keyName === "back") {
+							var page = document
+									.getElementsByClassName('ui-page-active')[0], pageid = page ? page.id
+									: "";
+
+							switch (pageid) {
+							case "mainPage":
+								console.log("pop from main, exiting");
+								tizen.application.getCurrentApplication()
+										.exit();
+								break;
+							case "menuPage":
+								console.log("pop from menu to mainPage");
+								tau.changePage("#mainPage");
+								break;
+							case "routesMain":
+								console.log("pop from routes main to menuPage");
+								tau.changePage("#menuPage");
+								break;
+							}
+						}
+					});
+
 };
+function googleInit() {
+	console.log("google was inited");
+}
---
+++ js/menu.js
@@ -0,0 +1,106 @@
+var page = document.querySelector("#mainPage");
+var handler = document.querySelector("#menuButton");
+console.log(handler);
+
+var menuPage = document.querySelector("#menuPage"), selector = document
+		.getElementById("menuSelector"), clickBound, routesPopup = document
+		.querySelector("#routesPopup"), routesMain = document
+		.querySelector("#routesMain");
+/**
+ * pagebeforeshow event handler Do preparatory works and adds event listeners
+ */
+
+function menuButtonClickedHandler() {
+	console.log("menu clicked");
+	document.removeEventListener("rotarydetent", mapRotaryListener);
+
+	tau.changePage(menuPage);
+}
+function menuItemClickedHandler(event) {
+	var target = event.target;
+
+	if (target.classList.contains("ui-selector-indicator"))
+		menuItemWasSelected(parseInt($("#menuSelector > [data-index]").attr(
+				"data-index")));
+	console.log("clicked menu item");
+}
+
+function mapRotaryListener(ev) {
+	var dir = ev.detail.direction;
+	if (dir === "CW") {
+		map.zoomIn();
+
+	} else if (dir === "CCW") {
+
+		map.zoomOut();
+
+	} else {
+		alert("SMTH WRONG");
+	}
+}
+
+handler.addEventListener("click", menuButtonClickedHandler);
+page.addEventListener( "pagebeforeshow",function() {
+	console.log("showing MAIN PAGE");
+	document.addEventListener("rotarydetent", mapRotaryListener);
+});
+page.addEventListener( "pagebeforehide",function() {
+	console.log("HIDING MAIN PAGE");
+	document.removeEventListener("rotarydetent", mapRotaryListener);
+});
+
+
+menuPage.addEventListener("pagebeforeshow", function() {
+
+	clickBound = menuItemClickedHandler.bind(null);
+	tau.widget.Selector(selector);
+	selector.addEventListener("click", clickBound, false);
+
+	/*
+	 * var radius = window.innerHeight / 2 * 0.8;
+	 * 
+	 * clickHandlerBound = menuClickedHandler.bind(null);
+	 * handler.addEventListener("click", clickHandlerBound); if
+	 * (tau.support.shape.circle) { selector = tau.widget.Selector(elSelector, {
+	 * itemRadius : radius }); }
+	 */
+});
+menuPage.addEventListener("pagebeforehide", function() {
+	selector.removeEventListener("click", clickBound, false);
+
+	/*
+	 * handler.removeEventListener("click", clickHandlerBound); if
+	 * (tau.support.shape.circle) { selector.destroy(); }
+	 */
+});
+routesMain.addEventListener("pagebeforeshow", function() {
+	console.log("openning page routes main");
+});
+routesMain.addEventListener("pagebeforehide", function() {
+	console.log("hiding page routes main");
+});
+
+function closeMenu() {
+	console.log("closing menu");
+	tau.changePage(mainPage);
+}
+var MENU_ITEMS = {
+	BACK : 0,
+	SEARCH : 1,
+	ROUTES : 2
+};
+function menuItemWasSelected(itemIndex) {
+	switch (itemIndex) {
+	case MENU_ITEMS.BACK:
+		closeMenu();
+		break;
+	case MENU_ITEMS.SEARCH:
+		break;
+	case MENU_ITEMS.ROUTES:
+		tau.changePage("#routesMain");
+		// tau.openPopup(routesPopup);
+
+		break;
+	}
+	console.log(itemIndex);
+}
